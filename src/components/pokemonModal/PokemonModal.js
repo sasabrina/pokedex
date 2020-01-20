4edbfe8e9dbe8c52c02c54c8bc5fb3906201ea23
sabrina <sabrinaealvarez@gmail.com>
2020-01-20 10:06:35 -0300

    fetchAbilities modified

--- src/components/pokemonModal/PokemonModal.js
+++ src/components/pokemonModal/PokemonModal.js
@@ -1,34 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import {fetchAbilities} from '../../helpers/index'
 import './PokemonModal.scss'
-let data1 = []
 
 const PokemonModal = ({name, id, abilities}) => {
     const [abilitiesData, setAbilitiesData] = useState([])
 
-    const fetchAbylity = async url => {
-        await fetch(url)
-        .then(res => res.json())
-        .then(res => {
-            const {names, flavor_text_entries} = res
-            let data = {
-                en: {name: names[2].name, flavor_text: flavor_text_entries[21].flavor_text},
-                es: {name: names[4].name, flavor_text: flavor_text_entries[4].flavor_text}
-            }
-            
-            data1.push(data)
-        })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     const getAbilities = () =>{
-        abilities.map(a => fetchAbylity(a.ability.url))
+        fetchAbilities(abilities, setAbilitiesData)
     }
 
     useEffect(()=> {
         getAbilities()
-        setAbilitiesData(data1)
-    }, [getAbilities])
+    }, [])
 
-    // eslint-disable-next-line no-lone-blocks
     {console.log(abilitiesData)}
 
     // let ablitiesName = abilities.map((a, i) => (<p key={i}>{a.ability.name}</p>))
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -18,4 +18,20 @@ const fetchPokemons = async setter => {
     setter(pokemons)
 }
 
-export {fetchPokemons}
+const fetchAbilities = (arr, setter) => {
+    let data = []
+    arr.map(async a =>{
+        await fetch(a.ability.url)
+        .then(res => res.json())
+        .then(res => {
+            let {names, flavor_text_entries} = res;
+            data.push({
+                en: {name: names[2].name, flavor_text: flavor_text_entries[21].flavor_text},
+                es: {name: names[4].name, flavor_text: flavor_text_entries[4].flavor_text}
+            })
+        })
+    })
+    setter(data)
+}
+
+export {fetchPokemons, fetchAbilities}
